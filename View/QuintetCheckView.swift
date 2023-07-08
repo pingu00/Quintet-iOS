24fea2caa1bf6f9e5b9a4b2c9404a9ae131676ba
pingu00 <58204852+pingu00@users.noreply.github.com>
2023-07-09 01:29:12 +0900

    초기 설정 View 파일 생성

---
+++ View/QuintetCheckView.swift
@@ -0,0 +1,20 @@
+//
+//  QuintetCheckView.swift
+//  Quintet-iOS
+//
+//  Created by Phil on 2023/07/09.
+//
+
+import SwiftUI
+
+struct QuintetCheckView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct QuintetCheckView_Previews: PreviewProvider {
+    static var previews: some View {
+        QuintetCheckView()
+    }
+}
---
+++ View/RecordView.swift
@@ -0,0 +1,20 @@
+//
+//  RecordView.swift
+//  Quintet-iOS
+//
+//  Created by Phil on 2023/07/09.
+//
+
+import SwiftUI
+
+struct RecordView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RecordView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecordView()
+    }
+}
