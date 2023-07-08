923470061c3f33fe240ff2703aa9358b562856b3
pingu00 <58204852+pingu00@users.noreply.github.com>
2023-07-09 01:03:06 +0900

    Initial commit

---
+++ HomeView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Quintet-iOS
+//
+//  Created by Phil on 2023/07/09.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
---
+++ Quintet_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Quintet_iOSApp.swift
+//  Quintet-iOS
+//
+//  Created by Phil on 2023/07/09.
+//
+
+import SwiftUI
+
+@main
+struct Quintet_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+}
