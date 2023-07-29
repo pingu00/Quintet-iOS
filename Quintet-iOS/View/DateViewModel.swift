33ab36d38332a54d4bb43236fc511bde7c58c1c9
Yjooon <yjk7557@naver.com>
2023-07-30 07:13:53 +0900

    Merge pull request #8 from UMC-Quintet/feat/MenuView
    [Refactor] 코드 리팩토링, StatisticsCellViewModel, DateViewModel 파일 분리

---
+++ Quintet-iOS/View/DateViewModel.swift
@@ -0,0 +1,47 @@
+//
+//  DateViewModel.swift
+//  Quintet-iOS
+//
+//  Created by 김영준 on 2023/07/27.
+//
+
+import SwiftUI
+
+class DateViewModel: ObservableObject {
+    @Published var selectedYear = Calendar.current.component(.year, from: Date()){
+        didSet {
+            updateStartOfWeek()
+        }
+    }
+    @Published var selectedMonth = Calendar.current.component(.month, from: Date()){
+        didSet {
+            updateStartOfWeek()
+        }
+    }
+    
+    @Published var startOfWeek = Calendar.current.date(from: Calendar.current.dateComponents([.yearForWeekOfYear, .weekOfYear], from: Date()))!
+    
+    var selectedDate: Date {
+        Calendar.current.date(from: DateComponents(year: selectedYear, month: selectedMonth, day: 1))!
+    }
+
+    var weekButtonText: Text {
+        let formattedStartDate = Utilities.formatYearMonthDay(startOfWeek)
+        let formattedEndDate = Utilities.formatYearMonthDay(Calendar.current.date(byAdding: .day, value: 6, to: startOfWeek)!)
+        
+        return Text("\(formattedStartDate) - \(formattedEndDate)")
+    }
+
+    private func updateStartOfWeek() {
+        let calendar = Calendar.current
+        startOfWeek = calendar.date(from: calendar.dateComponents([.yearForWeekOfYear, .weekOfYear], from: selectedDate))!
+    }
+        
+    var yearMonthButtonText: Text {
+        Text("\(Utilities.formatNum(selectedYear))년 \(selectedMonth)월")
+    }
+    
+    var yearButtonText: Text {
+        Text("\(Utilities.formatNum(selectedYear))년")
+    }
+}
