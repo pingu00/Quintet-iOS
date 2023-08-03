52981b94615862cc4ef77fe6bcde2c82dc341a3e
pingu00 <58204852+pingu00@users.noreply.github.com>
2023-08-03 20:17:39 +0900

    [Feat] 메뉴뷰 UI 구현 ( 회원 , 비회원 ) 화면설계 ver 1.1

--- Quintet-iOS/View/MenuView.swift
+++ Quintet-iOS/View/MenuView.swift
@@ -10,117 +10,122 @@ import SwiftUI
 struct MenuView: View {
     @Environment(\.dismiss) private var dismiss
     @State private var isNotiOn = false
+    @StateObject var vm = CoreDataViewModel()
+    @State private var alarm = Date()
+    @State private var hasLogin = false
     var body: some View {
         ZStack{
             Color("Background").ignoresSafeArea(.all)
-            List {
-                Section {
-                    Button(action: {}){
-                        HStack{
-                            Text("시간대 설정")
-                            Spacer()
-                            Image(systemName: "chevron.right")
-                                .foregroundColor(Color("DarkGray"))
-                        }
+            VStack {
+                Spacer(minLength: 20)
+                Group{
+                    if hasLogin {
+                        Text(vm.userName)
+                            .fontWeight(.bold)
+                        + Text("님")
+                        Text("안녕하세요!")
                     }
-                    Toggle("알림", isOn: $isNotiOn)
-                        .toggleStyle(SwitchToggleStyle(tint: Color("DarkQ")))
-                } header: {
-                    Text("알림설정")
-                        .foregroundColor(.black)
-                        .font(.system(size: 18))
-                        .fontWeight(.medium)
-                        .padding(.bottom,9)
-                }
-                Section {
-                    Button(action: {}){
-                        HStack{
-                            Text("기존 데이터 불러오기 / 내보내기")
-                            Spacer()
-                            Image(systemName: "chevron.right")
-                                .foregroundColor(Color("DarkGray"))
-                        }
-                    }
-                    Button(action: {}){
-                        HStack{
-                            Text("데이터 초기화")
-                            Spacer()
-                            Image(systemName: "chevron.right")
-                                .foregroundColor(Color("DarkGray"))
-                        }
-                    }
-                } header: {
-                    Text("데이터")
-                        .foregroundColor(.black)
-                        .font(.system(size: 18))
-                        .fontWeight(.medium)
-                        .padding(.bottom,9)
-                }
-                Section {
-                    Button(action: {}){
-                        HStack{
-                            Text("리뷰 남기러 가기")
-                            Spacer()
-                            Image(systemName: "chevron.right")
-                                .foregroundColor(Color("DarkGray"))
-                        }
-                    }
-                    Button(action: {}){
-                        HStack{
-                            Text("문의하기")
-                            Spacer()
-                            Image(systemName: "chevron.right")
-                                .foregroundColor(Color("DarkGray"))
-                        }
-                    }
-                    Button(action: {}){
-                        HStack{
-                            Text("업데이트 소식")
-                            Spacer()
-                            Image(systemName: "chevron.right")
-                                .foregroundColor(Color("DarkGray"))
-                        }
-                    }
-                    Button(action: {}){
-                        HStack{
-                            Text("이용 약관")
-                            Spacer()
-                            Image(systemName: "chevron.right")
-                                .foregroundColor(Color("DarkGray"))
-                        }
+                    else {
+                        Text("로그인 \n 해주세요!")
+                            .fontWeight(.medium)
+                            .multilineTextAlignment(.center)
                     }
                 }
-                Section {
-                    Button(action: {}){
-                        HStack{
-                            Text("로그아웃")
-                            Spacer()
-                            Image(systemName: "chevron.right")
-                                .foregroundColor(Color("DarkGray"))
+                .font(.system(size: 24))
+                Form {
+                    Section {
+                        if hasLogin { // 회원 일때
+                            Button(action: {}){
+                                HStack{
+                                    Text("프로필 편집")
+                                    Spacer()
+                                    Image(systemName: "chevron.right")
+                                        .foregroundColor(Color("DarkGray"))
+                                }
+                            }
+                            Button(action: {}){
+                                HStack{
+                                    Text("로그아웃")
+                                    Spacer()
+                                    Image(systemName: "chevron.right")
+                                        .foregroundColor(Color("DarkGray"))
+                                }
+                            }
+                            Button(action: {}){
+                                HStack{
+                                    Text("계정 탈퇴")
+                                    Spacer()
+                                    Image(systemName: "chevron.right")
+                                        .foregroundColor(Color("DarkGray"))
+                                }
+                            }
+                        }
+                        else { // 비회원 일때
+                            Button(action: {print("로그인 화면으로 이동")}){
+                                HStack{
+                                    Text("로그인")
+                                    Spacer()
+                                    Image(systemName: "chevron.right")
+                                        .foregroundColor(Color("DarkGray"))
+                                }
+                            }
+                        }
+                    } header: {
+                        Text("계정")
+                            .foregroundColor(.black)
+                            .font(.system(size: 18))
+                            .fontWeight(.medium)
+                            .padding(.bottom,9)
+                    }
+                    Section {
+                        Toggle("알림", isOn: $isNotiOn)
+                            .toggleStyle(SwitchToggleStyle(tint: Color("DarkQ")))
+                        DatePicker( "시간대 설정", selection: $alarm,
+                                    displayedComponents: .hourAndMinute)
+                        .environment(\.locale, Locale(identifier: "ko_KR"))
+                    } header: {
+                        Text("알림설정")
+                            .foregroundColor(.black)
+                            .font(.system(size: 18))
+                            .fontWeight(.medium)
+                            .padding(.bottom,9)
+                    }
+                   
+                    Section {
+                        Button(action: {}){
+                            HStack{
+                                Text("리뷰 남기러 가기")
+                                Spacer()
+                                Image(systemName: "chevron.right")
+                                    .foregroundColor(Color("DarkGray"))
+                            }
+                        }
+                        Button(action: {}){
+                            HStack{
+                                Text("문의하기")
+                                Spacer()
+                                Image(systemName: "chevron.right")
+                                    .foregroundColor(Color("DarkGray"))
+                            }
+                        }
+                        Button(action: {}){
+                            HStack{
+                                Text("이용 약관")
+                                Spacer()
+                                Image(systemName: "chevron.right")
+                                    .foregroundColor(Color("DarkGray"))
+                            }
                         }
                     }
-                    Button(action: {}){
-                        HStack{
-                            Text("계정 탈퇴")
-                            Spacer()
-                            Image(systemName: "chevron.right")
-                                .foregroundColor(Color("DarkGray"))
-                        }
-                    }
-                } header: {
-                    Text("계정")
-                        .foregroundColor(.black)
-                        .font(.system(size: 18))
-                        .fontWeight(.medium)
-                        .padding(.bottom,9)
+                    
+                    
                 }
-                
-            }
-            .font(.system(size: 16))
-            .environment(\.defaultMinListRowHeight, 58)
-            .foregroundColor(.black)
-            .background(Color("Background"))
+                .font(.system(size: 16))
+                .environment(\.defaultMinListRowHeight, 58)
+                .foregroundColor(.black)
+                .background(Color("Background"))
             .scrollContentBackground(.hidden)
+            }
         }
         .navigationBarBackButtonHidden(true)
         .toolbar {
