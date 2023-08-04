013ec153e8e429bda6e20de2e5dcc46c5e5c991a
Yjooon <yjk7557@naver.com>
2023-08-04 16:39:16 +0900

    [Feat] 변경된 CoreDate에 따라 1차 구현

--- Quintet-iOS/View/StatisticsCellViewModel.swift
+++ Quintet-iOS/View/StatisticsCellViewModel.swift
@@ -8,55 +8,26 @@
 import SwiftUI
 
 class StatisticsCellViewModel: ObservableObject {
-    @Published private var workPoint = 1
-    @Published private var healthPoint = 1
+    @Published private var workPoint = 5
+    @Published private var healthPoint = 3
     @Published private var familyPoint = 1
-    @Published private var relationshipPoint = 1
+    @Published private var relationshipPoint = 4
     @Published private var assetPoint = 1
+    
+    let startDate = Calendar.current.date(from: DateComponents(year: 2023, month: 8, day: 1))!
+    let endDate = Calendar.current.date(from: DateComponents(year: 2023, month: 8, day: 7))!
+
     
     private let coreDataViewModel: CoreDataViewModel
     
     init(coreDataViewModel: CoreDataViewModel) {
         self.coreDataViewModel = coreDataViewModel
-        updateWeekValuesFromCoreData()
+        updateValuesFromCoreData(startDate: startDate, endDate: endDate)
     }
     
-    func updateWeekValuesFromCoreData() {
-        let coreDataValues = coreDataViewModel.getQuintetData(for: Date())
+    func updateValuesFromCoreData(startDate: Date, endDate: Date) {
+        _ = coreDataViewModel.getQuintetData(from: startDate, to: endDate)
         
-        workPoint = Int(coreDataValues?.workPoint ?? 3)
-        healthPoint = Int(coreDataValues?.healthPoint ?? 2)
-        familyPoint = Int(coreDataValues?.familyPoint ?? 3)
-        relationshipPoint = Int(coreDataValues?.relationshipPoint ?? 5)
-        assetPoint = Int(coreDataValues?.assetPoint ?? 1)
-    }
-    
-    func updateMonthValuesFromCoreData() {
-        let coreDataValues = coreDataViewModel.getQuintetData(for: Date())
-        
-        workPoint = Int(coreDataValues?.workPoint ?? 1)
-        healthPoint = Int(coreDataValues?.healthPoint ?? 7)
-        familyPoint = Int(coreDataValues?.familyPoint ?? 2)
-        relationshipPoint = Int(coreDataValues?.relationshipPoint ?? 5)
-        assetPoint = Int(coreDataValues?.assetPoint ?? 5)
-    }
-    
-    func updateYearValuesFromCoreData() {
-//        let calendar = Calendar.current
-//        guard let startDate = calendar.date(from: DateComponents(year: year, month: 1, day: 1)),
-//              let endDate = calendar.date(from: DateComponents(year: year, month: 12, day: 31)) else {
-//            // 날짜를 계산하는 데 실패한 경우
-//            return
-//        }
-
-//        let coreDataValues = coreDataViewModel.getQuintetData(for: startDate, endDate: endDate)
-        let coreDataValues = coreDataViewModel.getQuintetData(for: Date())
-        
-        workPoint = Int(coreDataValues?.workPoint ?? 6)
-        healthPoint = Int(coreDataValues?.healthPoint ?? 2)
-        familyPoint = Int(coreDataValues?.familyPoint ?? 2)
-        relationshipPoint = Int(coreDataValues?.relationshipPoint ?? 4)
-        assetPoint = Int(coreDataValues?.assetPoint ?? 1)
     }
     
     var totalPoint: Int {
--- Quintet-iOS/View/StatisticsView.swift
+++ Quintet-iOS/View/StatisticsView.swift
@@ -123,13 +123,13 @@ struct StatisticsView: View {
                 }
                 .onChange(of: selectedOption) { newValue in
                     if newValue == 1 {
-                        statisticsCellViewModel.updateWeekValuesFromCoreData()
+                        statisticsCellViewModel.updateValuesFromCoreData(startDate: dateViewModel.startOfWeek, endDate: Calendar.current.date(byAdding: .day, value: 6, to: dateViewModel.startOfWeek)!)
                     }
                     else if newValue == 2{
-                        statisticsCellViewModel.updateMonthValuesFromCoreData()
+                        statisticsCellViewModel.updateValuesFromCoreData(startDate: dateViewModel.selectedDate, endDate: Calendar.current.date(byAdding: .month, value: 1, to: dateViewModel.selectedDate)!)
                     }
                     else {
-                        statisticsCellViewModel.updateYearValuesFromCoreData()
+                        statisticsCellViewModel.updateValuesFromCoreData(startDate: dateViewModel.selectedDate, endDate: Calendar.current.date(byAdding: .year, value: 1, to: dateViewModel.selectedDate)!)
                     }
                 }
                 
@@ -182,11 +182,40 @@ struct StatisticsView: View {
                 VStack{
                     RoundedRectangle(cornerRadius: 20)
                         .fill(Color.white)
-                        .frame(width: 330, height: 200)
+                        .frame(width: 330, height: 280)
                         .overlay(
-                            Text(".")
+                            HStack{
+                                Spacer()
+                                switch selectedOption {
+                                case 1:
+                                    Text("일은 삶의 필수적인 부분이고 많은 보상을 가져다줄 수 있지만, 삶에서 다른 요소들의 중요성도 인식하는 것이 중요합니다. 건강, 관계, 가족 및 자산을 소홀히 하면 전반적인 웰빙과 행복감에 영향을 미칠 수 있는 중대한 결과를 초래할 수 있습니다. 자신의 일에 열정을 갖는 것은 자연스러운 일이지만, 전반적인 성취감과 행복감에 기여할 수 있는 다른 중요한 요소가 있다는 것을 인식하는 것이 중요합니다.")
+                                        .font(.system(size: 16))
+                                        .multilineTextAlignment(.leading)
+                                        .frame(width: 250, height: 300)
+                                        .lineSpacing(1)
+                                        .padding(35)
+                                case 2:
+                                    Text("건강한 삶을 위해 충분한 관심을 보이는 모습이 보입니다. 하지만 건강에 힘쓰는 만큼 일과 가족에도 집중해보는 것이 어떨까요? 가족과 소중한 사람들과의 소통과 시간을 늘리며, 일상의 작은 순간들을 함께 나누는 것이 중요합니다. 자산과 관계에도 균형을 유지하면서 일과 가족을 동시에 행복하게 발전시키는 것을 추구해야합니다. 균형 잡힌 삶을 위해 모든 요소에 균등한 노력을 기울여보세요!")
+                                        .font(.system(size: 16))
+                                        .multilineTextAlignment(.leading)
+                                        .frame(width: 250, height: 300)
+                                        .lineSpacing(1)
+                                        .padding(35)
+                                case 3:
+                                    Text("관계에 많은 노력을 기울였다는 점이 돋보입니다. 전체적으로 5요소의 균형이 나쁘지 않지만, 자산에도 관심을 가져주세요! 자산을 키우기 위해 더 많은 관심과 노력을 기울이는 것이 도움이 될 것입니다. 투자, 저축, 재정관리에 더 신경을 쓰면서 미래를 위한 준비를 강화해보세요. 모든 영역에 균형을 유지하면서 더욱 행복하고 풍요로운 삶을 즐기시기를 바랍니다.")
+                                        .font(.system(size: 16))
+                                        .multilineTextAlignment(.leading)
+                                        .frame(width: 250, height: 300)
+                                        .lineSpacing(1)
+                                        .padding(35)
+                                default:
+                                    Text("")
+                                }
+                                Spacer()
+                            }
                         )
                 }
+
             }
         }
         .navigationBarBackButtonHidden(true)
