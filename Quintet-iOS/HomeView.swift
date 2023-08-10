5892e0e9d43be262ee23ff78532a1378e795afb1
pingu00 <58204852+pingu00@users.noreply.github.com>
2023-08-10 22:49:33 +0900

    [FIx] Navigation View 삭제

--- Quintet-iOS/HomeView.swift
+++ Quintet-iOS/HomeView.swift
@@ -12,188 +12,159 @@ struct HomeView: View {
     @StateObject private var dateViewModel = DateViewModel()
     var body: some View {
         VStack{
-            NavigationView {
+            ScrollView {
                 VStack{
-                    ScrollView {
+                    HStack{
+                        Text(viewModel.getTodayString())
+                            .font(.system(size: 18))
+                            .fontWeight(.medium)
+                            .padding(.leading, 5)
+                        Spacer()
+                    }
+                    Divider()
+                        .frame(height: 0.6)
+                        .background(Color("LightGray"))
+                        .padding(.vertical)
+                    // MARK: - 요일 Section
+                    HStack{
+                        ForEach(0..<7, id: \.self) { index in
+                            WeekCellView(viewModel: viewModel,date: viewModel.getDate(index: index), quintetData: viewModel.getSelectDayData(date: viewModel.getDate(index: index)), is_selected: viewModel.getDate(index: index) == viewModel.selectDay).onTapGesture {
+                                viewModel.setSelectDay(index: index)
+                            }
+                        }
+                    }
+                    .padding()
+                    
+                    // MARK: - 선택한 날짜에 퀸텟 기록이 있으면 보여주고, 없으면 없다는 메세지를 보여줌
+                    if let quintetData = viewModel.getSelectDayData(date: viewModel.selectDay){
+                        HappinessView(quintetData: quintetData)
+                    }
+                    else{
+                        Text("퀸텟체크 기록이 없습니다.")
+                    }
+                    Divider()
+                        .frame(height: 0.6)
+                        .background(Color("LightGray"))
+                        .padding(.vertical)
+                    
+                    // MARK: - 오늘의 퀸텟 체크 Section
+                    ZStack{
+                        NavigationLink(destination: QuintetCheckView()){
+                            RoundedRectangle(cornerRadius: 10)
+                                .foregroundColor(.white)
+                        }
+                        HStack{
+                            Text("오늘의 \n퀸텟체크")
+                                .font(.system(size: 28))
+                                .fontWeight(.bold)
+                                .padding(.horizontal, 40)
+                                .padding(.vertical, 30)
+                            Spacer()
+                            VStack{
+                                Spacer()
+                                Image(systemName: "arrow.right")
+                                    .tint(.black)
+                                    .font(.title)
+                            }
+                            .padding(.horizontal, 40)
+                            .padding(.vertical, 30)
+                        }
+                    }.onAppear{
+                        print("update QuintetData")
+                        viewModel.updateValuesFromCoreData(startDate: viewModel.startDate, endDate: viewModel.endDate)
+                    }
+                    .padding(.bottom)
+                    
+                    // MARK: - 분석확인, 기록확인 Section
+                    ZStack{
+                        RoundedRectangle(cornerRadius: 10)
+                            .foregroundColor(.white)
                         VStack{
-                            HStack{
-                                Text(viewModel.getTodayString())
-                                    .font(.system(size: 18))
-                                    .fontWeight(.medium)
-                                    .padding(.leading, 5)
-                                Spacer()
-                            }
-                            Divider()
-                                .frame(height: 0.6)
-                                .background(Color("LightGray"))
-                                .padding(.vertical)
-                            // MARK: - 요일 Section
-                            HStack{
-                                ForEach(0..<7, id: \.self) { index in
-                                    WeekCellView(viewModel: viewModel,date: viewModel.getDate(index: index), quintetData: viewModel.getSelectDayData(date: viewModel.getDate(index: index)), is_selected: viewModel.getDate(index: index) == viewModel.selectDay).onTapGesture {
-                                        viewModel.setSelectDay(index: index)
-                                    }
-                                }
-                            }
-                            .padding()
-                            
-                            // MARK: - 선택한 날짜에 퀸텟 기록이 있으면 보여주고, 없으면 없다는 메세지를 보여줌
-                            if let quintetData = viewModel.getSelectDayData(date: viewModel.selectDay){
-                                HappinessView(quintetData: quintetData)
-                            }
-                        }
-                        .padding()
-                        
-                        // MARK: - 선택한 날짜에 퀸텟 기록이 있으면 보여주고, 없으면 없다는 메세지를 보여줌
-                        if let happinessData = viewModel.selectDateData?.happiness{
-                            HappinessView(happinessData: happinessData)
-                        }
-                        else{
-                            Text("퀸텟체크 기록이 없습니다.")
-                        }
-                        Divider()
-                            .frame(height: 0.6)
-                            .background(Color("LightGray"))
-                            .padding(.vertical)
-                        
-                        // MARK: - 오늘의 퀸텟 체크 Section
-                        ZStack{
-                            NavigationLink(destination: QuintetCheckView()){
-                                RoundedRectangle(cornerRadius: 10)
-                                    .foregroundColor(.white)
-                            }
-                            HStack{
-                                Text("오늘의 \n퀸텟체크")
-                                    .font(.system(size: 28))
-                                    .fontWeight(.bold)
-                                    .padding(.horizontal, 40)
-                                    .padding(.vertical, 30)
-                                Spacer()
-                                VStack{
-                                    Spacer()
-                                    Image(systemName: "arrow.right")
-                                        .tint(.black)
-                                        .font(.title)
-                                }
-                            }.onAppear{
-                                print("update QuintetData")
-                                viewModel.updateValuesFromCoreData(startDate: viewModel.startDate, endDate: viewModel.endDate)
-                            }
-                            .padding(.bottom)
-                            
-                            // MARK: - 분석확인, 기록확인 Section
                             ZStack{
-                                RoundedRectangle(cornerRadius: 10)
-                                    .foregroundColor(.white)
-                                VStack{
-                                    ZStack{
-                                        NavigationLink(destination: {StatisticsView()}){
-                                            RoundedRectangle(cornerRadius: 10)
-                                                .foregroundColor(.white)
-                                        }
-                                        HStack(alignment: .center){
-                                            VStack(alignment: .leading){
-                                                Text("지난주 분석 확인하기")
-                                                    .fontWeight(.semibold)
-                                                    .font(.system(size: 18))
-                                                    .padding(.bottom, 0.5)
-                                                Text("\(viewModel.getDateString(date: viewModel.previousStartDate)) - \(viewModel.getDateString(date: viewModel.previousEndDate))")
-                                                    .font(.system(size: 14))
-                                            }.padding()
-                                            Spacer()
-                                            Image(systemName: "chevron.right")
-                                                .tint(.black)
-                                                .font(.title)
-                                                .padding()
-                                        }
-                                    }.padding()
-                                    
-                                    Divider()
-                                        .background(Color("LightGray"))
-                                        .padding(.horizontal)
-                                    ZStack{
-                                        NavigationLink(destination: RecordView()){
-                                            RoundedRectangle(cornerRadius: 10)
-                                                .foregroundColor(.white)
-                                        }
-                                        HStack(alignment: .center){
-                                            Text("기록 확인하기")
-                                                .fontWeight(.semibold)
-                                                .font(.system(size: 18))
-                                                .padding(.bottom, 0.5)
-                                            Text("2023. 06. 04 - 2023. 06. 10")
-                                                .font(.system(size: 14))
-                                        }.padding()
-                                        Spacer()
-                                        Image(systemName: "chevron.right")
-                                            .tint(.black)
-                                            .font(.title)
-                                            .padding()
-                                    }
-                                }.padding()
-                                
-                                Divider()
-                                    .background(Color("LightGray"))
-                                    .padding(.horizontal)
-                                ZStack{
-                                    NavigationLink(destination: RecordView()){
-                                        RoundedRectangle(cornerRadius: 10)
-                                            .foregroundColor(.white)
-                                    }
-                                    HStack(alignment: .center){
-                                        Text("기록 확인하기")
+                                NavigationLink(destination: {StatisticsView()}){
+                                    RoundedRectangle(cornerRadius: 10)
+                                        .foregroundColor(.white)
+                                }
+                                HStack(alignment: .center){
+                                    VStack(alignment: .leading){
+                                        Text("지난주 분석 확인하기")
                                             .fontWeight(.semibold)
                                             .font(.system(size: 18))
-                                            .padding()
-                                        
-                                        Spacer()
-                                        Image(systemName: "chevron.right")
-                                            .tint(.black)
-                                            .font(.title)
-                                            .padding()
-                                        
+                                            .padding(.bottom, 0.5)
+                                        Text("\(viewModel.getDateString(date: viewModel.previousStartDate)) - \(viewModel.getDateString(date: viewModel.previousEndDate))")
+                                            .font(.system(size: 14))
                                     }.padding()
+                                    Spacer()
+                                    Image(systemName: "chevron.right")
+                                        .tint(.black)
+                                        .font(.title)
+                                        .padding()
                                 }
-                            }.padding(.bottom, 10)
-                        }
-                        
-                        // MARK: - 추천 영상 Section
-                        HStack{
-                            Text("추천 영상으로\n행복을 챙겨요")
-                                .fontWeight(.semibold)
-                                .font(.system(size: 30))
-                                .padding()
-                            Spacer()
-                        }.padding(.vertical, 10)
-                        
-                        VStack{
-                            HStack{
-                                //각 영상 cell을 등록
-                                VideoCellView(videoURL: "video1", videoTitle: "건강한 삶을 위한 규칙적인 식습관", thumbnail: "video1")
-                                    .padding(.leading)
-                                Spacer()
-                                    .frame(width: 10)
-                                VideoCellView(videoURL: "video2", videoTitle: "인간관계에서 편해지는 법", thumbnail: "video2")
-                                    .padding(.trailing)
-                            }.padding(.bottom)
-                            VideoCellView(videoURL: "video3", videoTitle: "월급의 몇 %를 저축하고 있나요?", thumbnail: "video3")
+                            }.padding()
+                            
+                            Divider()
+                                .background(Color("LightGray"))
                                 .padding(.horizontal)
-                        }
+                            ZStack{
+                                NavigationLink(destination: RecordView()){
+                                    RoundedRectangle(cornerRadius: 10)
+                                        .foregroundColor(.white)
+                                }
+                                HStack(alignment: .center){
+                                    Text("기록 확인하기")
+                                        .fontWeight(.semibold)
+                                        .font(.system(size: 18))
+                                        .padding()
+                                    
+                                    Spacer()
+                                    Image(systemName: "chevron.right")
+                                        .tint(.black)
+                                        .font(.title)
+                                        .padding()
+                                    
+                                }.padding()
+                            }
+                        }.padding(.bottom, 10)
                     }
-                    .padding(20)
-                }
-            }
-            .background(Color("Background"))
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    NavigationLink(destination: MenuView()) {
-                        Image(systemName: "line.3.horizontal").padding()
-                            .tint(.black)
+                    
+                    // MARK: - 추천 영상 Section
+                    HStack{
+                        Text("추천 영상으로\n행복을 챙겨요")
+                            .fontWeight(.semibold)
+                            .font(.system(size: 30))
+                            .padding()
+                        Spacer()
+                    }.padding(.vertical, 10)
+                    
+                    VStack{
+                        HStack{
+                            //각 영상 cell을 등록
+                            VideoCellView(videoURL: "video1", videoTitle: "건강한 삶을 위한 규칙적인 식습관", thumbnail: "video1")
+                                .padding(.leading)
+                            Spacer()
+                                .frame(width: 10)
+                            VideoCellView(videoURL: "video2", videoTitle: "인간관계에서 편해지는 법", thumbnail: "video2")
+                                .padding(.trailing)
+                        }.padding(.bottom)
+                        VideoCellView(videoURL: "video3", videoTitle: "월급의 몇 %를 저축하고 있나요?", thumbnail: "video3")
+                            .padding(.horizontal)
                     }
                 }
+                .padding(20)
             }
-            .navigationBarBackButtonHidden(true)
         }
+        .navigationBarBackButtonHidden(true)
+        .background(Color("Background"))
+        .toolbar {
+            ToolbarItem(placement: .navigationBarTrailing) {
+                NavigationLink(destination: MenuView()) {
+                    Image(systemName: "line.3.horizontal").padding()
+                        .tint(.black)
+                }
+            }
+        }
+        
+        
     }
 }
 
