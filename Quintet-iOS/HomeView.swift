4a3d3b657a55d4e4555a7604097c7b392aaf1c6d
Phil_9 <58204852+pingu00@users.noreply.github.com>
2023-08-10 22:19:39 +0900

    Revert "[Refactor] CoreData 사용하도록 코드 리팩토링"

--- Quintet-iOS/HomeView.swift
+++ Quintet-iOS/HomeView.swift
@@ -7,39 +7,45 @@
 
 import SwiftUI
 
+class HomeViewModel: ObservableObject {
+    @Published var selectDayIndex = 6
+    @Published var selectDateData: HappinessInfo?
+    var dateManager = DateManager()
+    let dummyData = DummyDataManager.getDummyData()
+    
+    func selectDay(index: Int) {
+        selectDayIndex = index
+        selectDateData = dummyData[index]
+    }
+}
+
 struct HomeView: View {
     @StateObject private var viewModel = HomeViewModel()
-    @StateObject private var dateViewModel = DateViewModel()
+    
     var body: some View {
         VStack{
-            NavigationView {
-                VStack{
-                    ScrollView {
-                        VStack{
-                            HStack{
-                                Text(viewModel.getTodayString())
-                                    .font(.system(size: 18))
-                                    .fontWeight(.medium)
-                                    .padding(.leading, 5)
-                                Spacer()
-                            }
-                            Divider()
-                                .frame(height: 0.6)
-                                .background(Color("LightGray"))
-                                .padding(.vertical)
-                            // MARK: - 요일 Section
-                            HStack{
-                                ForEach(0..<7, id: \.self) { index in
-                                    WeekCellView(viewModel: viewModel,date: viewModel.getDate(index: index), quintetData: viewModel.getSelectDayData(date: viewModel.getDate(index: index)), is_selected: viewModel.getDate(index: index) == viewModel.selectDay).onTapGesture {
-                                        viewModel.setSelectDay(index: index)
-                                    }
-                                }
-                            }
-                            .padding()
+            VStack{
+                ScrollView {
+                    VStack{
+                        HStack{
+                            Text(viewModel.dateManager.getTodayText())
+                                .font(.system(size: 18))
+                                .fontWeight(.medium)
+                                .padding(.leading, 5)
+                            Spacer()
+                        }
+                        Divider()
+                            .frame(height: 0.6)
+                            .background(Color("LightGray"))
+                            .padding(.vertical)
+                        // MARK: - 요일 Section
+                        HStack{
                             
-                            // MARK: - 선택한 날짜에 퀸텟 기록이 있으면 보여주고, 없으면 없다는 메세지를 보여줌
-                            if let quintetData = viewModel.getSelectDayData(date: viewModel.selectDay){
-                                HappinessView(quintetData: quintetData)
+                            ForEach(0..<7, id: \.self) { index in
+                                WeekCellView(happinessInfo: viewModel.dummyData[index], is_selected: viewModel.selectDayIndex == index)
+                                    .onTapGesture {
+                                        viewModel.selectDay(index: index)
+                                    }
                             }
                             else{
                                 Text("퀸텟체크 기록이 없습니다.")
@@ -73,49 +79,25 @@ struct HomeView: View {
                                         .tint(.black)
                                         .font(.title)
                                 }
-                            }.onAppear{
-                                print("update QuintetData")
-                                viewModel.updateValuesFromCoreData(startDate: viewModel.startDate, endDate: viewModel.endDate)
+                                .padding(.horizontal, 40)
+                                .padding(.vertical, 30)
                             }
-                            .padding(.bottom)
-                            
-                            // MARK: - 분석확인, 기록확인 Section
-                            ZStack{
-                                RoundedRectangle(cornerRadius: 10)
-                                    .foregroundColor(.white)
-                                VStack{
-                                    ZStack{
-                                        NavigationLink(destination: {StatisticsView()}){
-                                            RoundedRectangle(cornerRadius: 10)
-                                                .foregroundColor(.white)
-                                        }
-                                        HStack(alignment: .center){
-                                            VStack(alignment: .leading){
-                                                Text("지난주 분석 확인하기")
-                                                    .fontWeight(.semibold)
-                                                    .font(.system(size: 18))
-                                                    .padding(.bottom, 0.5)
-                                                Text("\(viewModel.getDateString(date: viewModel.previousStartDate)) - \(viewModel.getDateString(date: viewModel.previousEndDate))")
-                                                    .font(.system(size: 14))
-                                            }.padding()
-                                            Spacer()
-                                            Image(systemName: "chevron.right")
-                                                .tint(.black)
-                                                .font(.title)
-                                                .padding()
-                                        }
-                                    }.padding()
-                                    
-                                    Divider()
-                                        .background(Color("LightGray"))
-                                        .padding(.horizontal)
-                                    ZStack{
-                                        NavigationLink(destination: RecordView()){
-                                            RoundedRectangle(cornerRadius: 10)
-                                                .foregroundColor(.white)
-                                        }
-                                        HStack(alignment: .center){
-                                            Text("기록 확인하기")
+                        }
+                        .padding(.bottom)
+                        
+                        // MARK: - 분석확인, 기록확인 Section
+                        ZStack{
+                            RoundedRectangle(cornerRadius: 10)
+                                .foregroundColor(.white)
+                            VStack{
+                                ZStack{
+                                    NavigationLink(destination: {StatisticsView()}){
+                                        RoundedRectangle(cornerRadius: 10)
+                                            .foregroundColor(.white)
+                                    }
+                                    HStack(alignment: .center){
+                                        VStack(alignment: .leading){
+                                            Text("지난주 분석 확인하기")
                                                 .fontWeight(.semibold)
                                                 .font(.system(size: 18))
                                                 .padding(.bottom, 0.5)
@@ -195,12 +177,10 @@ struct HomeView: View {
     }
 }
 
-
 // MARK: - 요일 cell
 struct WeekCellView: View{
-    let viewModel: HomeViewModel
-    let date : Date
-    let quintetData : QuintetData?
+    let dateManager = DateManager()
+    let happinessInfo : HappinessInfo
     var is_selected: Bool //해당 셀이 선택 되었는지
     
     var body: some View{
@@ -209,11 +189,11 @@ struct WeekCellView: View{
                 .foregroundColor(getForegroundColor())
                 .frame(width: 40, height: 90)
             VStack{
-                Text(viewModel.getWeekDayString(date: date))
+                Text(dateManager.get_week(dateStr: happinessInfo.date))
                     .foregroundColor(getTextColor())
                     .padding(.bottom)
                     .fontWeight(.light)
-                Text(viewModel.getDayString(date: date))
+                Text(dateManager.get_day(dateStr: happinessInfo.date))
                     .foregroundColor(getTextColor())
                     .fontWeight(.semibold)
             }
@@ -225,20 +205,19 @@ struct WeekCellView: View{
         if is_selected{
             return Color("DarkQ")
         } else {
-            if(viewModel.isSameDay(date1: date, date2: Date())) { //오늘 날짜일 경우
+            if(dateManager.is_today(dateStr: happinessInfo.date)) {
                 return Color("DarkGray")
             }
             else {
-                if quintetData != nil{
-                    return Color("LightGray2")
-                }else{return .clear}
+                if happinessInfo.happiness != nil{return Color("LightGray2")}
+                else{return .clear}
             }
         }
     }
     
     //요일 박스의 글자 색을 결정한다. (흰색 또는 검은색)
     private func getTextColor() -> Color {
-        if is_selected || viewModel.isSameDay(date1: date, date2: Date()){
+        if is_selected || dateManager.is_today(dateStr: happinessInfo.date){
             return Color("White")
         } else {
             return Color("Black")
@@ -282,19 +261,19 @@ struct HappinessCell: View{
 
 // MARK: - 5가지 퀸텟 지수 Cell을 모아둔 view. 퀸텟 체크 기록이 있어야 나타난다.
 struct HappinessView: View{
-    let quintetData : QuintetData
+    let happinessData : [Int]
     
     var body: some View{
         HStack{
-            HappinessCell(type: "일", value: Int(quintetData.workPoint))
+            HappinessCell(type: "일", value: happinessData[0])
             Spacer()
-            HappinessCell(type: "건강", value: Int(quintetData.healthPoint))
+            HappinessCell(type: "건강", value: happinessData[1])
             Spacer()
-            HappinessCell(type: "가족", value: Int(quintetData.familyPoint))
+            HappinessCell(type: "가족", value: happinessData[2])
             Spacer()
-            HappinessCell(type: "관계", value: Int(quintetData.relationshipPoint))
+            HappinessCell(type: "관계", value: happinessData[3])
             Spacer()
-            HappinessCell(type: "자산", value: Int(quintetData.assetPoint))
+            HappinessCell(type: "자산", value: happinessData[4])
         }.padding(.horizontal)
     }
 }
