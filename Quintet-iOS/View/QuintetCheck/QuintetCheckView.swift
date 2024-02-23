cc5155422e4136c70fa9c6ada8a6f3ccf6357a62
pingu00 <58204852+pingu00@users.noreply.github.com>
2024-02-23 11:09:07 +0900

    Update QuintetCheckView.swift

--- Quintet-iOS/View/QuintetCheck/QuintetCheckView.swift
+++ Quintet-iOS/View/QuintetCheck/QuintetCheckView.swift
@@ -13,6 +13,7 @@ struct QuintetCheckView: View {
     @Environment(\.dismiss) private var dismiss
     @State private var hasAddNote = false
     @State private var isComplete = false
+    private let isMember = KeyChainManager.read(forkey: .isNonMember) != "true"
     
     var body: some View {
         ZStack{
@@ -90,10 +91,9 @@ struct QuintetCheckView: View {
                     Spacer()
                     Button(action: {
                         vm.updateQuintetData()
-                        dismiss()
                         //MARK: - if 로그인 토크 보유 -> API 통해서 post
+                        if isMember {
                             NetworkManager.shared.postCheckData(parameters: [
-                                "user_id": 2,
                                 "work_deg": vm.workPoint,
                                 "health_deg": vm.healthPoint,
                                 "family_deg": vm.familyPoint,
@@ -105,7 +105,8 @@ struct QuintetCheckView: View {
                                 "relationship_doc": vm.relationshipNote,
                                 "money_doc": vm.assetNote,
                             ])
-                    
+                        }
+                        dismiss()
                     }){
                         RoundedRectangle(cornerRadius: 20)
                             .fill(Color("DarkQ"))
