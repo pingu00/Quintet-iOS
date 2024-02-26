0253e81464e860f12ea45a0097de84b45c761d77
YeongJoon <yjk7557@naver.com>
2024-02-26 22:28:31 +0900

    서버에서 데이터 값 없으면 다른 메시지를 출력하지 못하는 오류 수정

--- Quintet-iOS/View/StatisticsView.swift
+++ Quintet-iOS/View/StatisticsView.swift
@@ -165,7 +165,7 @@ struct StatisticsView: View {
                         .padding(.top, 40)
                 }
                 .padding(20)
-                if isLogin ? statisticsCellViewModel_login.maxPoint == 0 : statisticsCellViewModel.maxPoint == 0 {
+                if isLogin ? statisticsCellViewModel_login.maxPoint.isNaN || statisticsCellViewModel_login.maxPoint == 0 : statisticsCellViewModel.maxPoint == 0 {
                     VStack{
                         Text("기록된 데이터가 없어요...")
                             .padding(.top, 20)
@@ -192,7 +192,8 @@ struct StatisticsView: View {
                                     Text("")
                                 }
                             }
-                            Text(isLogin ? statisticsCellViewModel.maxNoteArray.joined(separator: ", ") : statisticsCellViewModel_login.maxNoteArray.joined(separator: ", "))
+                            //MARK: - maxNote 없을 때 아래 텍스트 없어지면 다른거 출력해야해
+                            Text(isLogin ? statisticsCellViewModel_login.maxNoteArray.joined(separator: ", ") : statisticsCellViewModel.maxNoteArray.joined(separator: ", "))
                                 .bold()
                                 .font(.system(size: 26))
                                 .multilineTextAlignment(.center)
@@ -387,7 +388,7 @@ struct StatisticsCellView: View {
             }
             VStack(spacing: 4) {
                 Text(note).font(.system(size: 15))
-                Text(String(format: "%.1f", heightRatio * 100) + "%")
+                Text(heightRatio.isNaN ? "0.0%" : String(format: "%.1f", heightRatio * 100) + "%")
                     .font(.system(size: 11))
                     .foregroundColor(.gray)
             }
